4674c56478b9fa3622e29f3840424ae8239cef6a
blackstar01-dark <ecruz@outlook.com>
2025-07-09 08:17:33 -0600

    Componentes

---
+++ resources/views/livewire/cliente/create.blade.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Models\cliente;
+use App\Providers\RouteServiceProvider;
+
+
+
+use function Livewire\Volt\{state};
+
+
+layout('layouts.app');
+state([
+    
+])
+
+?>
+
+<div>
+    //
+</div>
